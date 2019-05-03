affc32f344f1b7230e4305be9a1d63fec3161f3a
seaiin <namefinal@gmail.com>
2019-05-03 21:09:35 +0700

    Submit 807-2

--- 807-Max_Increase_To_Keep_City_Skyline.cpp
+++ 807-Max_Increase_To_Keep_City_Skyline.cpp
@@ -1,21 +1,23 @@
+#include <algorithm>
+
 class Solution {
 public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
-        
-        vector<vector<int>> max (100, vector<int>(100));
+
+        vector<int> max_row (vector<int>(100));
+        vector<int> max_column (vector<int>(100));
         int res = 0;
         
         for(int i = 0; i < grid.size(); i++) {
-
             for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j] > max[0][i]) max[0][i] = grid[i][j];
-                if (gird[i][j] > max[1][j]) max[1][j] = grid[i][j];
+                max_row[i] = max(grid[i][j], max_row[i]);
+                max_column[j] = max(grid[i][j], max_column[j]);
             }
         }
 
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[i].size(); j++) {
-                res += (max[0][i] < max[1][j]) ? max[0][i] - grid[i][j] : max[1][j] - grid[i][j];
+                res += (max_row[i] < max_column[j]) ? max_row[i] - grid[i][j] : max_column[j] - grid[i][j];
             }
         }
 
